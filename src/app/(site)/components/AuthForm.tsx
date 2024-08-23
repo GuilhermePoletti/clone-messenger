692aeaf8e82e87d06d5d9a910b907f82bf627281
Guilherme Poletti <gui.poletti@gmail.com>
2024-08-23 17:48:44 -0300

    "Updated .env file with Google OAuth credentials, modified AuthForm component to handle session and routing, fixed GitHub client secret in auth route, and added AuthContext to layout"

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import {
     Field,
@@ -14,13 +14,22 @@ import Input from "../../components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { toast } from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/users');
+        }
+    }, [session?.status, router]);
 
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN') {
@@ -49,6 +58,7 @@ const AuthForm = () => {
 
         if (variant === 'REGISTER') {
             axios.post('/api/register', data)
+                .then(() => signIn('credentials', data))
                 .catch(() => toast.error('algo deu errado'))
                 .finally(() => setIsLoading(false))
         }
@@ -65,9 +75,10 @@ const AuthForm = () => {
 
                     if (callback?.ok && !callback?.error) {
                         toast.success('Logado!')
+                        router.push('/users')
                     }
                 })
-                .finally(() => setIsLoading(false))
+                .finally(() => setIsLoading(false));
         }
     }
 
@@ -84,7 +95,7 @@ const AuthForm = () => {
                     toast.success('Logado!')
                 }
             })
-            .finally(() => setIsLoading(false))//1:53:01
+            .finally(() => setIsLoading(false));
     }
 
     return (
---
+++ src/app/components/EmptyState.tsx
@@ -0,0 +1,29 @@
+const EmptyState = () => {
+  return ( 
+    <div 
+      className="
+        px-4
+        py-10
+        sm:px-6
+        lg:px-8
+        h-full
+        flex
+        justify-center
+        items-center
+        bg-gray-100
+      "
+    >
+      <div className="text-center items-center flex flex-col">
+        <h3
+          className="
+
+          "
+        >
+          Selecione um chat ou comece uma nova conversa
+        </h3>
+      </div>  
+    </div>
+  );
+}
+ 
+export default EmptyState; //2:11:22
