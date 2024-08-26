32f06bcb8f6541309deeaed8a69c6fe0e1aa0dc3
Guilherme Poletti <48832384+GuilhermePoletti@users.noreply.github.com>
2024-08-26 14:10:38 -0300

    2:17:22

--- src/app/components/EmptyState.tsx
+++ src/app/components/EmptyState.tsx
@@ -16,7 +16,10 @@ const EmptyState = () => {
       <div className="text-center items-center flex flex-col">
         <h3
           className="
-
+            mt-2
+            text-2xl
+            font-semibold
+            text-gray-900
           "
         >
           Selecione um chat ou comece uma nova conversa
@@ -26,4 +29,4 @@ const EmptyState = () => {
   );
 }
  
-export default EmptyState; //2:11:22
+export default EmptyState;
