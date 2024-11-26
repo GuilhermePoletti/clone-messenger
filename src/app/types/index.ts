3fc53061bb0f120d331b1991efef9c9d942f8faa
Guilherme Poletti <48832384+GuilhermePoletti@users.noreply.github.com>
2024-11-26 16:31:32 -0300

    painel de conversas

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,1 +1,11 @@
-3:39:54
+import { Conversation, Message, User } from "@prisma/client";
+
+export type FullMessageType = Message & {
+  sender: User,
+  seen: User[]
+};
+
+export type FullConversationType = Conversation & {
+  users: User[],
+  messages: FullMessageType[],
+};
